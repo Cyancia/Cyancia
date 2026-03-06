0b34c1d58386013b2b619097e79918775ef0aa73
443eb9#C <443eb9@gmail.com>
2026-03-06 16:06:06 +0800

    Added current pixel color and blend mode nodes.

--- crates/cyancia_brush/src/render/graph.rs
+++ crates/cyancia_brush/src/render/graph.rs
@@ -13,7 +13,7 @@ use cyancia_shader_graph::{
 };
 use glam::{Vec2, Vec4};
 use iced_core::{Color, Element, color};
-use iced_widget::{Column, pick_list, space};
+use iced_widget::{Column, column, pick_list, space};
 use serde::{Deserialize, Serialize};
 use wesl::{VirtualResolver, Wesl};
 
@@ -23,6 +23,8 @@ pub fn brush_graph_storage() -> GraphDynamicInstancesStorage {
     storage.nodes.register::<PixelPosition>();
     storage.nodes.register::<OutputPixelColor>();
     storage.nodes.register::<PasteTextureNode>();
+    storage.nodes.register::<BlendColorNode>();
+    storage.nodes.register::<CurrentPixelColorNode>();
     storage
 }
 
@@ -145,15 +147,9 @@ impl StatelessCommonGraphNode for OutputPixelColor {
     }
 
     fn generate_code(&self, ctx: GraphNodeCodeGenContext) -> Result<String, GraphNodeCodeGenError> {
-        let layer = ctx.ident_generator.next_output();
-        let coord = ctx.ident_generator.next_output();
-
         Ok(format!(
             r#"
-            var {layer} = 0u;
-            var {coord} = vec2u(0u);
-            convert_pixel_to_tile(pixel_pos, &{layer}, &{coord});
-            textureStore(outputs[{layer}], {coord}, image::texture_unpack::pack_rgba8_texel({}));
+            textureStore(outputs[cur_layer], vec2u(cur_coord), image::texture_unpack::pack_rgba8_texel({}));
             "#,
             ctx.get_input(0)?
         ))
@@ -209,6 +205,147 @@ impl StatelessCommonGraphNode for PasteTextureNode {
         Ok(format!(
             "let {} = paste_texture({}, pixel_posf, {}, {}, {}, {});\n",
             output, tex, scale, rotation, translation, anchor
+        ))
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct CurrentPixelColorNode;
+
+impl StatelessCommonGraphNode for CurrentPixelColorNode {
+    fn name(&self) -> &'static str {
+        "Current Pixel Color"
+    }
+
+    fn input_slot_names(&self) -> &[&'static str] {
+        &[]
+    }
+
+    fn output_slot_names(&self) -> &[&'static str] {
+        &["Color"]
+    }
+
+    fn header_color(&self) -> Color {
+        color!(0xf279f0)
+    }
+
+    fn create_inputs(&self) -> Vec<GraphDefaultInputSlot> {
+        vec![]
+    }
+
+    fn create_outputs(&self) -> Vec<GraphDefaultOutputSlot> {
+        vec![GraphDefaultOutputSlot::new::<ColorType>()]
+    }
+
+    fn generate_code(
+        &self,
+        mut ctx: GraphNodeCodeGenContext,
+    ) -> Result<String, GraphNodeCodeGenError> {
+        Ok(format!(
+            "let {} = image::texture_unpack::unpack_rgba8_texel(textureLoad(outputs[cur_layer], vec2u(cur_coord)));",
+            ctx.get_output(0)?
+        ))
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct BlendColorNode;
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct BlendColorNodeState {
+    pub blend_mode: BlendMode,
+}
+
+#[derive(Clone)]
+pub enum BlendColorNodeMessage {
+    LiteralUpdate(ErasedGraphLiteralUpdateMessage),
+    SetBlendMode(BlendMode),
+}
+
+impl GraphNode for BlendColorNode {
+    type State = BlendColorNodeState;
+
+    type Message = BlendColorNodeMessage;
+
+    fn name(&self) -> &'static str {
+        "Blend Color"
+    }
+
+    fn default_state(&self) -> Self::State {
+        BlendColorNodeState {
+            blend_mode: BlendMode::Normal,
+        }
+    }
+
+    fn header_color(&self) -> Color {
+        color!(0x79ccf2)
+    }
+
+    fn create_inputs(&self, state: &Self::State) -> Vec<GraphDefaultInputSlot> {
+        vec![
+            GraphDefaultInputSlot::new::<ColorType>(Vec4::ZERO),
+            GraphDefaultInputSlot::new::<ColorType>(Vec4::ZERO),
+        ]
+    }
+
+    fn create_outputs(&self, state: &Self::State) -> Vec<GraphDefaultOutputSlot> {
+        vec![GraphDefaultOutputSlot::new::<ColorType>()]
+    }
+
+    fn view_inputs(
+        &self,
+        state: &Self::State,
+        ctx: GraphNodeInputsViewContext,
+    ) -> Element<'static, Self::Message, GraphTheme, GraphRenderer> {
+        let pick = pick_list(
+            BlendMode::ALL,
+            Some(state.blend_mode),
+            BlendColorNodeMessage::SetBlendMode,
+        );
+
+        column![pick]
+            .extend(ctx.view_all_inputs(
+                &["Src Color", "Dst Color"],
+                BlendColorNodeMessage::LiteralUpdate,
+            ))
+            .into()
+    }
+
+    fn view_outputs(
+        &self,
+        state: &Self::State,
+        ctx: GraphNodeOutputsViewContext,
+    ) -> Element<'static, Self::Message, GraphTheme, GraphRenderer> {
+        Column::with_children(ctx.view_all_outputs(&["Color"])).into()
+    }
+
+    fn update(
+        &self,
+        state: &mut Self::State,
+        message: Self::Message,
+        mut ctx: GraphNodeUpdateContext,
+    ) {
+        match message {
+            BlendColorNodeMessage::LiteralUpdate(message) => ctx.update_literal(message),
+            BlendColorNodeMessage::SetBlendMode(blend_mode) => state.blend_mode = blend_mode,
+        }
+    }
+
+    fn generate_code(
+        &self,
+        state: &Self::State,
+        mut ctx: GraphNodeCodeGenContext,
+    ) -> Result<String, GraphNodeCodeGenError> {
+        let src = ctx.get_input(0)?;
+        let dst = ctx.get_input(1)?;
+        let output = ctx.get_output(0)?;
+
+        Ok(format!(
+            "let {} = image::blend_modes::{}({}, {});\n",
+            output,
+            state.blend_mode.shader_func(),
+            src,
+            dst
         ))
     }
 }
