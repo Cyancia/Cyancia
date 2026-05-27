46e328dd8f5980613cd386fc35d60d330f50e13b
443eb9#C <443eb9@gmail.com>
2026-05-27 12:37:42 +0800

    Better node drag and select handling.

--- crates/cyancia_shader_graph/src/editor/mod.rs
+++ crates/cyancia_shader_graph/src/editor/mod.rs
@@ -1,7 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
 use gpui::{
-    Action, Bounds, Context, InteractiveElement, IntoElement, MouseButton, MouseDownEvent, MouseMoveEvent, MouseUpEvent, ParentElement, Pixels, Point, Render, SharedString, Styled, Window, div, prelude::FluentBuilder, px
+    Action, Bounds, Context, InteractiveElement, IntoElement, MouseButton, MouseDownEvent,
+    MouseMoveEvent, MouseUpEvent, ParentElement, Pixels, Point, Render, SharedString, Styled,
+    Window, div, prelude::FluentBuilder, px,
 };
 use gpui_component::{ActiveTheme, ElementExt, menu::ContextMenuExt};
 use schemars::JsonSchema;
@@ -21,6 +23,7 @@ pub struct GraphEditor<Data: GraphData> {
     graph: Graph<Data>,
     node_registry: GraphNodeRegistry<Data>,
     drag_state: Option<DragState>,
+    marquee_state: Option<MarqueeState>,
     selected_nodes: HashSet<GraphNodeId>,
 }
 
@@ -30,6 +33,7 @@ impl<Data: GraphData> GraphEditor<Data> {
             graph,
             node_registry,
             drag_state: None,
+            marquee_state: None,
             selected_nodes: HashSet::new(),
         }
     }
@@ -50,16 +54,12 @@ impl<Data: GraphData> GraphEditor<Data> {
         self.graph.add_boxed_node(pos, node);
     }
 
-    pub fn on_mouse_down(
+    pub fn on_left_mouse_down(
         &mut self,
         event: &MouseDownEvent,
         window: &mut Window,
         cx: &mut Context<Self>,
     ) {
-        if event.button != MouseButton::Left {
-            return;
-        }
-
         self.selected_nodes.clear();
         cx.notify();
     }
@@ -70,44 +70,56 @@ impl<Data: GraphData> GraphEditor<Data> {
         window: &mut Window,
         cx: &mut Context<Self>,
     ) {
+        if self.drag_state.is_some() {
+            self.node_drag(event, window, cx);
+        }
+    }
+
+    pub fn node_drag_start(
+        &mut self,
+        event: &MouseDownEvent,
+        window: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
+        if event.button != MouseButton::Left {
+            return;
+        }
+
+        self.drag_state = Some(DragState {
+            cursor_origin: window.mouse_position(),
+            node_origins: self
+                .selected_nodes
+                .iter()
+                .filter_map(|id| Some((id.clone(), self.graph.get_node(id)?.position)))
+                .collect(),
+        });
+    }
+
+    pub fn node_drag(
+        &mut self,
+        event: &MouseMoveEvent,
+        window: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
         if event.pressed_button != Some(MouseButton::Left) {
             return;
         }
 
-        match &self.drag_state {
-            Some(drag) => {
-                let offset = window.mouse_position() - drag.cursor_origin;
-                let node_offset = Point::new(offset.x.into(), offset.y.into());
+        let Some(drag) = &mut self.drag_state else {
+            return;
+        };
 
-                for (id, origin) in &drag.node_origins {
-                    let pos = *origin + node_offset;
-                    if let Some(node) = self.graph.get_node_mut(id) {
-                        node.position = pos;
-                    }
-                }
+        let offset = window.mouse_position() - drag.cursor_origin;
+        let node_offset = Point::new(offset.x.into(), offset.y.into());
 
-                cx.notify();
-            }
-            None => {
-                self.drag_state = Some(DragState {
-                    cursor_origin: window.mouse_position(),
-                    node_origins: self
-                        .selected_nodes
-                        .iter()
-                        .filter_map(|id| Some((id.clone(), self.graph.get_node(id)?.position)))
-                        .collect(),
-                });
+        for (id, origin) in &drag.node_origins {
+            let pos = *origin + node_offset;
+            if let Some(node) = self.graph.get_node_mut(id) {
+                node.position = pos;
             }
         }
-    }
 
-    pub fn on_mouse_up(
-        &mut self,
-        event: &MouseUpEvent,
-        window: &mut Window,
-        cx: &mut Context<Self>,
-    ) {
-        self.drag_state = None;
+        cx.notify();
     }
 
     pub fn get_node_state_mut<T: GraphNode<Data>>(
@@ -167,11 +179,13 @@ impl<Data: GraphData> Render for GraphEditor<Data> {
                                     editor.add_node_selection(node_id);
                                 } else if event.modifiers.control {
                                     editor.toggle_node_selection(node_id);
-                                } else {
+                                } else if !editor.selected_nodes.contains(&node_id) {
                                     editor.selected_nodes.clear();
                                     editor.add_node_selection(node_id);
                                 }
                             }
+
+                            editor.node_drag_start(event, window, cx);
 
                             cx.stop_propagation();
                         });
@@ -185,9 +199,8 @@ impl<Data: GraphData> Render for GraphEditor<Data> {
             .h_full()
             .children(nodes)
             .on_action(cx.listener(Self::on_add_node_action))
-            .on_mouse_down(MouseButton::Left, cx.listener(Self::on_mouse_down))
+            .on_mouse_down(MouseButton::Left, cx.listener(Self::on_left_mouse_down))
             .on_mouse_move(cx.listener(Self::on_mouse_move))
-            .on_mouse_up(MouseButton::Left, cx.listener(Self::on_mouse_up))
             .context_menu(move |menu, window, cx| {
                 let all_nodes = all_nodes.clone();
                 menu.submenu("Add Node", window, cx, move |mut menu, window, cx| {
@@ -208,4 +221,9 @@ impl<Data: GraphData> Render for GraphEditor<Data> {
 struct DragState {
     cursor_origin: Point<Pixels>,
     node_origins: HashMap<GraphNodeId, Point<f32>>,
+}
+
+struct MarqueeState {
+    cursor_origin: Point<Pixels>,
+    originally_selected: HashSet<GraphNodeId>,
 }
