95cd043d8bc6c64c2a7630c1cf32ab25178be2ad
443eb9#C <443eb9@gmail.com>
2026-05-28 00:02:30 +0800

    Added extra field to manually adjust color of random result.

--- crates/cyancia_utils/src/themed_color.rs
+++ crates/cyancia_utils/src/themed_color.rs
@@ -2,11 +2,14 @@ use std::panic::Location;
 
 use bevy_color::{Oklcha, Srgba};
 use gpui::{App, Rgba};
-use gpui_component::{animation::Lerp, Theme};
+use gpui_component::{Theme, animation::Lerp};
 
 #[macro_export]
 macro_rules! random_oklch {
-    ($struct_name:ty, $cx: expr) => {{
+    ($struct_name:ty, $cx:expr) => {
+        random_oklch!($struct_name, $cx, 0u32)
+    };
+    ($struct_name:ty, $cx:expr, $extra_offset:literal) => {{
         const CH: (f32, f32) = {
             let name = stringify!($struct_name).as_bytes();
             let mut i = 0;
@@ -15,6 +18,7 @@ macro_rules! random_oklch {
                 hash = (hash << 5).wrapping_sub(hash) + name[i] as u32;
                 i += 1;
             }
+            hash = (hash << 5).wrapping_sub(hash) + $extra_offset;
 
             const MIN_C: f32 = 0.05;
             const MAX_C: f32 = 0.15;
